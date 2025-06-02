c3c0f528e0054571fa9295848d1fc6a4ff57dced
Ana Carolina <84293579+AyaCarol@users.noreply.github.com>
2025-06-02 14:50:19 -0300

    modificação da tela de detalhes da OS para popup na tela principal

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,8 @@
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
+
+function togglePopup() {
+            const overlay = document.getElementById('popupOverlay');
+            overlay.classList.toggle('show');
+}
