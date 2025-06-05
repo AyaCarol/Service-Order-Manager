851514d4772f888da02231b685c42bbc97560dbe
Ana Carolina <84293579+AyaCarol@users.noreply.github.com>
2025-06-05 19:15:38 -0300

    melhorias de código para padronização de estilização

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,8 +1,25 @@
-document.getElementById('open_btn').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
-});
+window.addEventListener('DOMContentLoaded', () => {
+  fetch('sidebar.html')
+    .then(response => response.text())
+    .then(html => {
+      const container = document.getElementById('sidebar-container');
+      container.innerHTML = html;
 
-function togglePopup() {
-            const overlay = document.getElementById('popupOverlay');
-            overlay.classList.toggle('show');
-}
+      // Adiciona o CSS
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = 'css/sidebar.css';
+      document.head.appendChild(link);
+
+      // Pega a página atual
+      const currentPage = window.location.pathname.split("/").pop();
+      const sideItems = container.querySelectorAll('.side-item');
+
+      sideItems.forEach(item => {
+        const link = item.querySelector('a');
+        if (link && link.getAttribute('href') === currentPage) {
+          item.classList.add('active');
+        }
+      });
+    });
+});
